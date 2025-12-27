20ed80b0e1f181361c6e9b4138ed2225173b795a
ARJU RAHANGDALE <aarju.rahangdale@rungta.org>
2025-12-27 08:40:05 +0530

    Update author information in practical6_1.C

--- Practical 6/practical6_1.C
+++ Practical 6/practical6_1.C
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {      //PRACTICAL 6 :-Question no.1:-Print numbers from a to b using loops//
-    int a, b; //Aishwarya Sharma, ERP-10122//
+    int a, b; //Arju rahangdale, ERP-10012//
     printf("Enter the starting number (a): ");
     scanf("%d", &a);
     printf("Enter the ending number (b): ");
@@ -16,3 +16,4 @@ int main() {      //PRACTICAL 6 :-Question no.1:-Print numbers from a to b using
 //Enter the starting number (a): 1
 //Enter the ending number (b): 8
 //Numbers from 1 to 8: 1 2 3 4 5 6 7 8 
+
