df8afab5ee348b9cd889bc86ff6e5bc6f562b695
ARJU RAHANGDALE <aarju.rahangdale@rungta.org>
2025-12-27 08:59:15 +0530

    pattern printing program for user input
    Updated the program to print a pattern based on user input.

--- practical 7/practical7_3.c
+++ practical 7/practical7_3.c
@@ -1,10 +1,19 @@
-#include <stdio.h>
-int main() { printf("Arju_10012\n");
-    int i,j,n=4;
-    for(i=1;i<=n;i++) {
-        for(j=0;j<i;j++)
-            printf("%c",'A'+j);
+#include <stdio.h>  
+int main() {              //PRACTICAL 07: Q.no.3:-Print the pattern: A, AB, ABC, ABCD//
+    int n;                // Arju rahanagdale , 10012 //
+    printf("Enter number of lines: ");  
+    scanf("%d", &n);
+    for(int i = 1; i <= n; i++) {
+        for(int j = 0; j < i; j++) {
+            printf("%c", 'A' + j);
+        }
         printf("\n");
     }
     return 0;
-}
+} 
+//Output//
+// Enter number of lines: 4
+// A
+// AB
+// ABC
+// ABCD
