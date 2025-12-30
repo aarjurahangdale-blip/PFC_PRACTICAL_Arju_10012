f2479c378b447421b38847765c0105bdb187f5ab
ARJU RAHANGDALE <aarju.rahangdale@rungta.org>
2025-12-30 15:26:54 +0530

    Enhance practical9_3.c with comments and output
    Added comments and output example to the code.

--- Practical 9/practical9_3.c
+++ Practical 9/practical9_3.c
@@ -1,5 +1,6 @@
+// QUESTION: Sum of all even elements of an array
+//Arju Rahangdale 10012
 #include <stdio.h>
-
 int main() {
     int n, i, sum = 0;
     int arr[100];
@@ -16,8 +17,13 @@ int main() {
             sum += arr[i];
         }
     }
-
     printf("Sum of even elements = %d", sum);
 
     return 0;
 }
+
+/*OUTPUT: 
+Enter number of elements: 6
+10 15 20 33 40 7
+Sum of even elements = 70
+ */
